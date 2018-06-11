09c81de6d8092535a8ee7a98f64d4de25dc074c8
Claudiu94 <claudiu.zafi@gmail.com>
2018-06-11 08:17:26 +0300

    added changes for database communication

--- HealthChart/src/main/java/com/healthchart/controller/BaseController.java
+++ HealthChart/src/main/java/com/healthchart/controller/BaseController.java
@@ -1,6 +1,8 @@
 package com.healthchart.controller;
 
+import com.healthchart.service.TestComponent;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,9 @@ public class BaseController {
 	private static int counter = 0;
 	private static final String VIEW_INDEX = "index";
 	private final static org.slf4j.Logger logger = LoggerFactory.getLogger(BaseController.class);
+
+	@Autowired
+	private TestComponent testComponent;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
@@ -29,7 +34,7 @@ public class BaseController {
 	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
 	public String welcomeName(@PathVariable String name, ModelMap model) {
 
-		model.addAttribute("message", "Welcome " + name);
+		model.addAttribute("message", "Welcome " + name + " " + testComponent.getMyName());
 		model.addAttribute("counter", ++counter);
 		logger.debug("[welcomeName] counter : {}", counter);
 		return VIEW_INDEX;
---
+++ HealthChart/src/main/java/com/healthchart/service/TestComponent.java
@@ -0,0 +1,11 @@
+package com.healthchart.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestComponent {
+
+    public String getMyName() {
+        return "Claudiu";
+    }
+}
